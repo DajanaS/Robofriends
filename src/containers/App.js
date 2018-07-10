e54ec34f7bced67f3504a311a7e55d9790082052
DajanaS <dajana.sk@hotmail.com>
2018-07-10 10:30:26 +0200

    Provider and connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+import {setSearchField} from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+};
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchField: ''
+            robots: []
         }
     }
 
@@ -20,12 +34,9 @@ class App extends Component {
             .then(users => this.setState({robots: users}));
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    };
-
     render() {
-        const {robots, searchField} = this.state;
+        const {robots} = this.state;
+        const {searchField, onSearchChange} = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
@@ -34,7 +45,7 @@ class App extends Component {
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots}/>
@@ -45,4 +56,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
