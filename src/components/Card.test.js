ce133eeaa4ab6633821436e6ca8b178d62e89f72
DajanaS <dajana.sk@hotmail.com>
2018-07-26 09:20:29 +0200

    Snapshot testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,11 @@
-import {shallow, mount, render} from 'enzyme';
+import {shallow} from 'enzyme';
 import React from 'react';
 import Card from './Card';
 
 it('expect to render Card component', () => {
     expect(shallow(<Card/>).length).toEqual(1);
+});
+
+it('expect to render Card component, with snapshot', () => {
+    expect(shallow(<Card/>)).toMatchSnapshot();
 });
